7cc9331d37faa85b7d297a859e9f8ca31283b6bd
csbelisle <csbelisle@yahoo.ca>
2024-02-08 14:07:23 -0500

    maj fév.

---
+++ index.2aa79c06.js
@@ -0,0 +1,13 @@
+(function() {
+    var extlinks, i, nodeMap, source;
+    nodeMap = document.createAttribute("target");
+    nodeMap.value = "_blank";
+    extlinks = document.getElementsByTagName("a");
+    i = 0;
+    while(i < extlinks.length){
+        source = extlinks[i].getAttributeNode("href").value;
+        if (source.substring(0, 4) === "http") extlinks[i].setAttribute("target", "_blank");
+        i++;
+    }
+}).call(this);
+
