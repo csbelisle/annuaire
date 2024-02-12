2af12578cc77fe4f079ec60f8659fe606f81aee3
csbelisle <csbelisle@yahoo.ca>
2024-02-12 08:13:09 -0500

    modification

--- index.2aa79c06.js
+++ index.2aa79c06.js
@@ -1,5 +1,5 @@
 (function() {
-    var extlinks, i, nodeMap, source;
+    var extlinks, i, navigation, nodeMap, source;
     nodeMap = document.createAttribute("target");
     nodeMap.value = "_blank";
     extlinks = document.getElementsByTagName("a");
@@ -9,5 +9,7 @@
         if (source.substring(0, 4) === "http") extlinks[i].setAttribute("target", "_blank");
         i++;
     }
+    navigation = document.querySelector("nav");
+    navigation.innerHTML = "<a href='aperitifs.html'> Ap\xe9ritifs</a> <a href='culture.html'> Culture</a> <a href='musique.html'> &#10132; Musique</a> <a href='informatique.html'> Informatique</a> <a href='linuxetc.html'> &#10132; Linux etc.</a> <a href='webdev.html'> &#10132; D\xe9v. web</a> <a href='medias.html'> M\xe9dias</a> <a href='science.html'> Science</a> <a href='senneterre.html'> Senneterre</a> <a href='voyager.html'> Voyager</a>";
 }).call(this);
 
